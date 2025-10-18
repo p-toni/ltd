d02469cb3ad2774851219e31e8452900bbfee4cc
p-toni <--global>
2025-10-18 18:49:30 -0300

    chore: gate mobile layout behind flag

--- components/tactical-blog.tsx
+++ components/tactical-blog.tsx
@@ -1,1 +1,21 @@
-export { TacticalBlogWrapper as default } from '@/components/tactical-blog-wrapper'
+import type { Piece } from '@/lib/pieces'
+import { ENABLE_MOBILE_LAYOUT } from '@/lib/feature-flags'
+import { TacticalBlogDesktop } from '@/components/tactical-blog-desktop'
+import { TacticalBlogProvider } from '@/components/tactical-blog-provider'
+import { TacticalBlogWrapper } from '@/components/tactical-blog-wrapper'
+
+interface TacticalBlogProps {
+  pieces: Piece[]
+}
+
+export default function TacticalBlog({ pieces }: TacticalBlogProps) {
+  if (!ENABLE_MOBILE_LAYOUT) {
+    return (
+      <TacticalBlogProvider pieces={pieces}>
+        <TacticalBlogDesktop />
+      </TacticalBlogProvider>
+    )
+  }
+
+  return <TacticalBlogWrapper pieces={pieces} />
+}
---
+++ lib/feature-flags.ts
@@ -0,0 +1,1 @@
+export const ENABLE_MOBILE_LAYOUT = process.env.NEXT_PUBLIC_ENABLE_MOBILE_LAYOUT === 'true'
